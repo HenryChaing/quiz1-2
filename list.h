4bc502769ad72d8d95c0ee608b5bde9619941884
HenryChaing <n96121210@gs.ncku.edu.tw>
2024-03-07 01:28:21 +0800

    Add quicksort.c and list.h to project
    quicksort.c requires list.h for API services. The quicksort function
    is identical to that of quiz 1.

---
+++ list.h
@@ -0,0 +1,60 @@
+#include <stdlib.h>
+
+typedef struct __node {
+    struct __node *left, *right;
+    struct __node *next;
+    long value;
+} node_t;
+
+void list_add(node_t **list, node_t *node_t)
+{
+    node_t->next = *list;
+    *list = node_t;
+}
+
+node_t *list_tail(node_t **left)
+{
+    while ((*left) && (*left)->next)
+        left = &((*left)->next);
+    return *left;
+}
+
+int list_length(node_t **left)
+{
+    int n = 0;
+    while (*left) {
+        ++n;
+        left = &((*left)->next);
+    }
+    return n;
+}
+
+/*lead to mid node*/
+node_t *list_mid(node_t **left)
+{
+    int mid = list_length(left)/2 -1;
+    for (int i = 0; i < mid; i++)
+    {
+        left = &((*left)->next);
+    }    
+    return *left;
+}
+
+node_t *list_construct(node_t *list, int n)
+{
+    node_t *node = (node_t *) malloc(sizeof(node_t));
+    node->next = list;
+    node->value = n;
+    return node;
+}
+
+void list_free(node_t **list)
+{
+    node_t *node = (*list)->next;
+    while (*list) {
+        free(*list);
+        *list = node;
+        if (node)
+            node = node->next;
+    }
+}
